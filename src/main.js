2cc7f00f7c7838945d034de5a23b738683f6f4ef
Tina Wang <tina@tina.cafe>
2022-02-27 23:09:21 +0100

    🏗️ Change folder hierarchy

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from '@/App.vue'
 import { routes } from '@/routes.js'
-import '@/styles/tailwind.css'
-import '@/styles/base.postcss'
+import '@/assets/styles/tailwind.css'
+import '@/assets/styles/base.postcss'
 
 const app = createApp(App)
 
