24b4eda42aa780b3bf57c070ba4ca095645488e9
Tina Wang <fabzidentiste@gmail.com>
2022-08-06 13:04:24 +0200

    💄 Change shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,8 @@ module.exports = {
         '2xs': '.65rem',
       },
       boxShadow: {
-        '4': '0 2px 4px -1px rgba(0,0,0,.2),0 4px 5px 0 rgba(0,0,0,.14),0 1px 10px 0 rgba(0,0,0,.12)',
-        '6': '0 3px 5px -1px rgba(0,0,0,.2),0 6px 10px 0 rgba(0,0,0,.14),0 1px 18px 0 rgba(0,0,0,.12)',
+        '4': '0 2px 4px -1px rgba(0,0,0,.1),0 4px 5px 0 rgba(0,0,0,.07),0 1px 10px 0 rgba(0,0,0,.06)',
+        '6': '0 3px 5px -1px rgba(0,0,0,.1),0 6px 10px 0 rgba(0,0,0,.07),0 1px 18px 0 rgba(0,0,0,.06)',
         '8': '0 5px 5px -3px rgba(0,0,0,.1),0 8px 10px 1px rgba(0,0,0,.07),0 3px 14px 2px rgba(0,0,0,.06)',
         '10': '0 6px 6px -3px rgba(0,0,0,.2),0 10px 14px 1px rgba(0,0,0,.14),0 4px 18px 3px rgba(0,0,0,.12)',
         '12': '0 7px 8px -4px rgba(0, 0, 0, 0.2), 0 12px 17px 2px rgba(0, 0, 0, 0.14), 0 5px 22px 4px rgba(0, 0, 0, 0.12)',
